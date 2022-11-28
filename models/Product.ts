78ac7bfb0a3d71ede364be49b458e8638035a40b
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-28 19:40:55 -0400

    Add model Product

---
+++ models/Product.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import {
+  Model,
+  Optional,
+  DataTypes,
+  Sequelize
+} from 'sequelize';
+interface ProductAttributes {
+  title: string;
+  priceForCents: number;
+  description: string;
+  image: string;
+  readonly id: number;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+}
+type ProductCreationAttributes = Optional<
+  ProductAttributes,
+  'id' | 'createdAt' | 'updatedAt'
+>;
+class Product extends Model<
+  ProductAttributes,
+  ProductCreationAttributes
+> {
+  static initModel(sequelize: Sequelize) {
+    this.init(
+      {
+        title: DataTypes.STRING,
+        priceForCents: DataTypes.INTEGER,
+        description: DataTypes.STRING,
+        image: DataTypes.STRING,
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE,
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        }
+      },
+      {
+        sequelize,
+        freezeTableName: true,
+        tableName: 'products'
+      }
+    );
+  }
+  public id!: number;
+
+  public title!: string;
+
+  public priceForCents!: number;
+
+  public description!: string;
+
+  public image!: string;
+
+  public readonly createdAt!: Date;
+
+  public readonly updatedAt!: Date;
+}
+
+export { Product };
