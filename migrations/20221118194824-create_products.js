1a3c8650cd121a3263be869dd89a600f685f8483
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-28 19:38:13 -0400

    Add migrate product

---
+++ migrations/20221118194824-create_products.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        type: DataTypes.UUID
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      priceForCents: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      description: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      image: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.fn('NOW')
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('products');
+  }
+};
